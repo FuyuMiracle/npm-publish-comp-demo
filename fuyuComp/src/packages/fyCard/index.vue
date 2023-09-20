d56c7f28cc08fc8e3380459267910425394c9caf
fuyuMiracle <643164248@qq.com>
2023-09-20 15:19:41 +0800

    第二次提交

--- fuyuComp/src/packages/fyCard/index.vue
+++ fuyuComp/src/packages/fyCard/index.vue
@@ -1,9 +1,22 @@
 <template>
-	<div class="fy-card">
-		<el-button>我是 fyCard 组件</el-button>
-	</div>
+	<fyConfigProvider :locale="locale">
+		<div class="fy-card">
+			<el-button>我是 fyCard 组件</el-button>
+			<el-pagination
+				v-model:current-page="currentPage"
+				v-model:page-size="pageSize"
+				:page-sizes="[100, 200, 300, 400]"
+				layout="total,sizes,prev,pager,next,jumper"
+			></el-pagination>
+		</div>
+	</fyConfigProvider>
 </template>
 <script lang="ts" setup>
-import { ElButton } from "element-plus";
+import { ref, inject } from "vue";
+import fyConfigProvider from "../fyConfigProvider/index";
+import { ElButton, ElPagination } from "element-plus";
+const locale = inject("$locale");
+const currentPage = ref(4);
+const pageSize = ref(100);
 </script>
 <style lang="scss" scoped></style>
--- fuyuComp/src/packages/fyList/index.vue
+++ fuyuComp/src/packages/fyList/index.vue
@@ -1,8 +1,5 @@
 <template>
-	<div class="fy-list">
-		<el-button>我是 fyList 组件3</el-button>
-		<el-divider />
-	</div>
+	<div class="fy-list">我是 fyList 组件</div>
 </template>
 <script lang="ts" setup></script>
 <style lang="scss" scoped></style>
